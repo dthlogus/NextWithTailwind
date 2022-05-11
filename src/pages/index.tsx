a29e9156d8850dfa757be51b19bfb413fb6d04b2
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-05-11 14:42:27 -0300

    TODO List with Next and Tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col bg-purple-600 justify-center items-center text-white h-screen bg-gradient-to-tr from-purple-500 to-yellow-600">
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  );
+}
