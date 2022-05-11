f2770c26ed93cd4ad83b0eaa42032b50c8a4ae0c
Matheus Ribeiro <matheus.junio159@gmail.com>
2022-05-11 15:29:46 -0300

    model tarefa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Tarefa from "../model/Tarefa";
+
 export default function Home() {
+  let tarefa: Tarefa = new Tarefa(1, "Exemplo tarefa");
+  tarefa = tarefa.altenarStatus();
+
   return (
     <div className="flex flex-col bg-purple-600 justify-center items-center text-white h-screen bg-gradient-to-tr from-purple-500 to-yellow-600">
-      <span>1</span>
-      <span>2</span>
-      <span>3</span>
+      <span>{tarefa.id}</span>
+      <span>{tarefa.descricao}</span>
+      <span>{tarefa.concluida ? "Sim" : "Não"}</span>
     </div>
   );
 }
